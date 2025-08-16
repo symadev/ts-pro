3bef2d9d6e4a76e68303b79373b79b0627a58e48
syma sultana <symasultana2002@gmail.com>
2025-08-16 12:37:01 +0600

    added examples of from, input, usestate

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Button from './Component/Button';
 function App() {
   return (
     <div className="App">
-     <Button label='Click me'/>
+     
      <Button  onClick={() =>alert("Click again")} label='Click me Again'/> 
       
     </div>
--- src/Component/Button.tsx
+++ src/Component/Button.tsx
@@ -1,17 +1,80 @@
+import { useState } from "react";
 
 
 interface ButtonProps {
-    label: string;
+    label: string | number | boolean
     onClick?: () => void;
+    something?: boolean;
 }
-//   {/* here need to be assign both file the label and onClick with the props */}
 
- const Button:React.FC<ButtonProps> = (props) =>{
+
+
+interface Books {
+    name: string;
+    price: number;
+
+}
+
+//   {/* here need to be assign both file the label and onClick with the props */}
+// this is how ew make the component reusable
+const Button: React.FC<ButtonProps> = (props) => {
+    const { label, onClick } = props;
+
+    //using the hook now
+    // const [value, setValue] = useState(1) //---> this process in in implicit process
+    const [value, setValue] = useState<number>(1); //----> this is explicit process
+
+
+
+
+
+
+    const [books, setBooks] = useState<Books>({ name: "one", price: 10 }); //----> this is explicit process
+    const [from, setFrom] = useState<string | undefined>()
+   
+    //must putted here the default value 
+    //the initial value is 1 here    
+
+
+
+
+
+
+    //this is for the frominput 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFrom(e.target.value);
+    }
+
+
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
+
+
     return (
-        <button onClick={props.onClick} className="button">
-            {props.label}
-        </button>
-       
+        <div>
+
+            <h3>name {books.name} (RS{books.price})</h3>
+            <button onClick={() => setBooks({ name: "Two", price: 20 })} className="button">Update</button>
+
+            <h3>{value}</h3>
+            <button onClick={() => setValue(value + 1)} className="button">
+                {label}
+            </button>
+            <h1>{from}</h1>
+            <input onChange={handleChange} type="text" value={from} placeholder="enter your name" />
+            <h1>{from}</h1>
+            <form onSubmit={handleSubmit}>
+                {/* //onSubmit only work for the from */}
+                
+                <button type="submit">Submit</button>
+            </form>
+
+
+        </div>
+
     );
- }
- export default Button;
+}
+export default Button;
